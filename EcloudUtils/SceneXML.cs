e9a3cfecbdb220718c237ec8375d73683f960c4d
Brustar <yeals2@gmail.com>
2017-04-01 10:00:16 +0800

    Crestron.SimplSharp.CrestronXmlLinq

--- EcloudUtils/SceneXML.cs
+++ EcloudUtils/SceneXML.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Crestron.SimplSharp;
+using System.XML;
+using Crestron.SimplSharp.CrestronXmlLinq;
 
 namespace EcloudUtils
 {
     public class SceneXML
     {
+        private static string readXML(string filePath)
+		{
+			XElement xe = XElement.Load(filePath);
+			IEnumerable<XElement> elements = from ele in 
+				xe.Elements("dict").Elements("array").Elements("dict")
+				                                                select ele;
+			string ret = "[";
+			foreach(var e in elements)
+			{
+				string json = "{";
+				foreach (var l in e.Elements())
+				{
+					if (l.Name == "key")
+					{
+						json = json + "\"" + l.Value + "\":";
+					}
+					else if (l.Name == "true" || l.Name == "false")
+					{
+
+						json = json + "\"" + l.Name + "\",";
+					}
+					else
+					{
+						json = json + "\"" + l.Value + "\",";
+					}
+
+				}
+				json = json.Substring(0,json.Length-1) + "}";
+				ret = ret + json + ",";
+			}
+			return ret.Substring(0, ret.Length - 1) + "]";
+		}
     }
 }
